1d08a91d343d73792a6341e1b2a4db3cd01bedc4
Elias Ferguson <eferguson00@stuy.edu>
2018-09-16 22:33:44 -0400

    recommitted

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+   public static void main(String[] args){
+      System.out.println("Hello my name is Elias Ferguson, and I barely figured out how to do this. ");
+   }
+}
+//hopefully this works
